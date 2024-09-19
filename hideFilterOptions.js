2d3883feff70e90b5e8ad5895256a3ab6d10a9ab
salaandora <50085010+salaandora@users.noreply.github.com>
2024-09-19 10:22:23 -0400

    Update hideFilterOptions.js
    Added:
    - Cleared up variable names
    - Filter list

--- hideFilterOptions.js
+++ hideFilterOptions.js
@@ -1,13 +1,14 @@
 dashboard.on('widgetready', (d, args) => {
-	list = document.getElementsByClassName('list-item slf-back-hov')
-	$.each(list, function(index, element) {
- 		if(element.innerText === 'BK' || element.innerText === 'N\\A'){
+	hiddenFilters = ['BK', 'N\\A']
+	filters = document.getElementsByClassName('list-item slf-back-hov');
+	$.each(filters, function(index, element) {
+ 		if(hiddenFilters.includes(element.innerText.trim())){
 			$(element).css({
 				'display': 'none' })
 		}
 	});
-	items = document.getElementsByClassName('uc-ms-toprow')
-	$.each(items, function(index, element) {
+	selectAll = document.getElementsByClassName('uc-ms-toprow')
+	$.each(selectAll, function(index, element) {
 		if (index > 0) {
 			$(element).css({
 				'display': 'none' })
